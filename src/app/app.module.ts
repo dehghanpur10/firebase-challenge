23d7158cef5bebb97693216841a1357900cb4731
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-09-02 20:16:03 +0430

    edit environment variable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from '../environments/env';
+import { environment } from '../environments/environment.prod';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import {AngularFireAuthModule} from "@angular/fire/compat/auth";
 
